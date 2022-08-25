11a382e2eccd5a3df444c11a2d9eae6b5f5dac54
Lien-Chu Huang <liarn2001@gmail>
2022-08-25 23:30:35 +0200

    Edited script for cities-bucketlist and again

--- Javascript/cities-bucketlist.js
+++ Javascript/cities-bucketlist.js
@@ -3,40 +3,33 @@ let i = 0;
 let citiesArray = [];
 let cityName = ['cityName1', 'cityName2', 'cityName3', 'cityName4', 'cityName5']
 
-/* Set an Enter key press event to add the city button. */
-window.onkeypress = function (event) {
-    if (event.which == 13) {
-        addCities();
-    }
-}
-
-function addCities() {
-        // execute addArray function. 
-        addArray();
-        // Set input become empty after reading the input.
-        document.getElementById('getCityName').value = "";   
+/* Set an onclick event to add the city button. */
+document.getElementById('cityNamebtn').onclick = function () {
+    // execute addArray function. 
+    addArray();
+    // Set input become empty after reading the input.
+    document.getElementById('getCityName').value = "";
 }
 
 // Create a function to set the input to Array's index and set it to an element with a unique id.
 function addArray() {
     // Select the input element corresponding to the HTML and assign to array's index.
-    citiesArray[i] = document.getElementById('getCityName').value;
-    citiesArray.push(citiesArray[i]);
-
-    //Assign array's index to display output array.
+    citiesArray.push(document.getElementById('getCityName').value);
+    //Assign array's index to display output element.
     document.getElementById(cityName[i]).innerHTML = capitalize(citiesArray[i]);
-   
     // Keep the input within definition.
     i++;
-    if (i == citiesArray.length)
+    if (i == citiesArray.length + 1)
         i = 0;
 }
 
 // Create a function to sort the Array. 
-function sortCities() {
+document.getElementById('sortCitiesbtn').onclick = function () {
     for (let i = 0; i < cityName.length; i++) {
+        // Set the method let elements to sorts in < numerical < alphabetical.
         citiesArray.sort();
-        document.getElementById(cityName[i]).innerHTML = (i + 1) + ": " +  capitalize(citiesArray[i]);
+        // Listup array and set the sorted array to display output element.
+        document.getElementById(cityName[i]).innerHTML = (i + 1) + ": " + capitalize(citiesArray[i]);
     }
 }
 
